b2fbad2cadb3be9b2c8b4949a5fe9f6bcb97592f
chaveamin <aminchave@gmail.com>
2025-12-04 11:36:46 +0330

    vars

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function build() {
-  return src("assets/scss/**/**.scss").pipe(sass()).pipe(dest("assets/scss/"));
+  return src("assets/scss/**/*.scss").pipe(sass()).pipe(dest("assets/scss/"));
 }
 
 function watcher() {
-  watch(["assets/scss/**/**.scss"], build);
+  watch(["assets/scss/**/*.scss"], build);
 }
 
 exports.default = series(build, watcher);
